c04180100dd4c55388ca0eaace4498223e6e07d9
Dimitrios Charalampidis <dimitrios@charalampidis.pro>
2024-06-08 07:02:15 +0200

    CU-8694t1kwp - Add navbar headers

--- nextjs-template/app/page.tsx
+++ nextjs-template/app/page.tsx
@@ -1,13 +1,13 @@
 // @/app/page.tsx
 
-import NavbarControlledMenuHeader from '@/app/ui/headers/navbarControlledMenuHeader';
+import NavbarCustomizingTheActiveItemHeader from '@/app/ui/headers/navbarCustomizingTheActiveItemHeader';
 import DefaultMain from '@/app/ui/mains/defaultMain';
 import DefaultFooter from '@/app/ui/footers/defaultFooter';
 
 export default function Home() {
   return (
     <>
-      <NavbarControlledMenuHeader />
+      <NavbarCustomizingTheActiveItemHeader />
       <DefaultMain />
       <DefaultFooter />
     </>
