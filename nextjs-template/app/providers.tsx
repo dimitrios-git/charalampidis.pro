355d026c4ffe1c7bf4d44e10d87ca4b974ab0902
Dimitrios Charalampidis <dimitrios@charalampidis.pro>
2024-06-08 04:39:02 +0200

    CU-8694t1kwp - Avoid page reloads

--- nextjs-template/app/providers.tsx
+++ nextjs-template/app/providers.tsx
@@ -3,11 +3,14 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={router.push}>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         {children}
       </NextThemesProvider>
