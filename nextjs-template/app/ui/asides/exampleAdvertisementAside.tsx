6b7ca07300873343d5e19cf8acc10bc225d300d1
Dimitrios Charalampidis <dimitrios@charalampidis.pro>
2024-06-08 07:34:20 +0200

    CU-8694t1kwp - Add different colors for dark theme

--- nextjs-template/app/ui/asides/exampleAdvertisementAside.tsx
+++ nextjs-template/app/ui/asides/exampleAdvertisementAside.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ExampleAdvertisementAside = () => {
   return (
-    <aside className="my-4 p-4 bg-yellow-900 border border-yellow-500 rounded-md">
+    <aside className="my-4 p-4 dark:bg-yellow-900 bg-yellow-100 border border-yellow-500 rounded-md">
       <h2 className="text-lg font-semibold">Sponsored</h2>
       <p>Check out this related product or service!</p>
     </aside>
