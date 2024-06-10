2a71b5ab5dd9cc12250c9edf33cdfea661450462
Dimitrios Charalampidis <dimitrios@charalampidis.pro>
2024-06-11 00:58:33 +0200

    CU-8694t26u4 - Fix MyNextUIMain max width

--- next.js-app/app/ui/mains/myNextUIMain.tsx
+++ next.js-app/app/ui/mains/myNextUIMain.tsx
@@ -8,18 +8,20 @@ const MyNextUIMain = () => {
   const t = useTranslations('MyNextUIMain');
 
   return (
-    <div className="flex flex-col md:flex-row md:container mx-auto p-4 dark:text-gray-100">
-      <main className="flex-grow md:w-3/4">
-        <h2 className="text-3xl font-bold">{t('Title')}</h2>
-        <p className="m-2 text-gray-700 dark:text-gray-300">
-          {t('Paragraph-1')}
-        </p>
-        <p className="m-2 text-gray-700 dark:text-gray-300">
-          {t('Paragraph-2')}
-        </p>
-        <MyNextUISection />
-      </main>
-      <MyNextUIAside className="md:w-1/4 md:ml-4 mt-4 md:mt-0" />
+    <div className="max-w-screen-lg mx-auto p-4">
+      <div className="flex flex-col md:flex-row lg:container dark:text-gray-100">
+        <main className="flex-grow md:w-3/4">
+          <h2 className="text-3xl font-bold">{t('Title')}</h2>
+          <p className="m-2 text-gray-700 dark:text-gray-300">
+            {t('Paragraph-1')}
+          </p>
+          <p className="m-2 text-gray-700 dark:text-gray-300">
+            {t('Paragraph-2')}
+          </p>
+          <MyNextUISection />
+        </main>
+        <MyNextUIAside className="md:w-1/4 md:ml-4 mt-4 md:mt-0" />
+      </div>
     </div>
   );
 };
