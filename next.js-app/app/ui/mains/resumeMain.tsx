0a9de54e34bcf1cdfde0e104159a4ae6edd7f427
Dimitrios Charalampidis <dimitrios@charalampidis.pro>
2024-06-12 13:29:53 +0200

    CU-8694tz84u - Fix localeswitcher in dropdown, add resume messages

--- next.js-app/app/ui/mains/resumeMain.tsx
+++ next.js-app/app/ui/mains/resumeMain.tsx
@@ -11,7 +11,11 @@ const ResumeMain = () => {
       <div className="flex flex-col md:flex-row lg:container dark:text-gray-100">
         <main className="flex-grow md:w-3/4">
           <h1 className="text-3xl font-bold">{t('Title')}</h1>
-          <p className="m-2">{t('Paragraph1')}</p>
+          <p className="m-2">
+            {t.rich('Paragraph1', {
+              strong: (chunks) => <strong>{chunks}</strong>,
+            })}
+          </p>
         </main>
         <ResumeAside className="md:w-1/4 md:ml-4 mt-4 md:mt-0" />
       </div>
